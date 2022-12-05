7c2e233d288a2c3c8a3a6328ab9ba3e58afec498
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2022-12-05 08:39:56 +0100

    manip interne git

--- src/main/java/survey/backend/BackendApplication.java
+++ src/main/java/survey/backend/BackendApplication.java
@@ -14,4 +14,5 @@ public class BackendApplication {		// On appelle normalement l'api sur le port 8
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
 }
