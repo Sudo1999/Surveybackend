a094743f4721fdd3614e2f8b4730bc1c20b0fd88
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2022-12-15 19:46:24 +0100

    Authentication

---
+++ src/main/java/survey/backend/auth/ApiAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package survey.backend.auth;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint {
+// This class just returns HTTP code 401 Unauthorized when authentication fails,
+// overriding the default Spring’s redirection :
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
---
+++ src/main/java/survey/backend/exception/DisabledUserException.java
@@ -0,0 +1,10 @@
+package survey.backend.exception;
+
+public class DisabledUserException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+    public DisabledUserException(String msg) {
+        super(msg);
+    }
+
+}
